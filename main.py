b8a41db8e828539d7fa7add828b0ffa76f02e112
arushanovanz <nadin.spb@gmail.com>
2022-09-30 20:52:24 +0300

    added about.html

--- main.py
+++ main.py
@@ -11,6 +11,11 @@ def index():
     return render_template('index.html')
 
 
+@main.route('/about')
+def about():
+    return render_template('about.html')
+
+
 @main.route('/profile')
 @login_required
 def profile():
